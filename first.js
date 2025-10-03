de1904fd527d07f140e283dd7e67cc308b32aafa
Tanjimul Islam Tareq <engineertareqbd@gmail.com>
2025-10-04 00:12:12 +0600

    first lesson of js in console

---
+++ first.js
@@ -0,0 +1,18 @@
+console.log("I'm Tareq, A noob dev.");
+console.log("I'm trying to learn Javascript")
+myname = "Tanjimul Islam Tareq";
+console.log(myname);
+age = 25;
+console.log("As of 2025 My is : "+ age);
+priceofpc = 55547.697
+console.log("My Pc's Price is : " +priceofpc);
+x = null;
+console.log("Checking null : "+ null);
+isFollow = false;
+console.log = (isFollow);
+//Variable Rules
+// Js catch data type dynamically
+// = is a assingment oparetor that assign the value to the variable
+// varible are case sensative - exaple : tareq & Tareq both are diffrent variable
+// latter, digit & _ are allowed - even space is not allowed
+// Variable Rules
